9f3caccd20b53131efb8956b301129715817691d
Luis Correa <luiscorrea874@gmail.com>
2024-09-21 18:44:01 -0500

    third commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/discount_react/',
+  base: 'https://luiscorrea9.github.io/discount_react/',
   plugins: [react()],
 })
